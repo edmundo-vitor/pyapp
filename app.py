2c391b390435e3c8a9ca6d878c71a4a4c32ec729
Edmundo Vitor <edmundovitor1414@hotmail.com>
2023-04-13 13:44:23 -0300

    teste CI/CD

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Jenkins ECR ECS deploy</h1>"
+    return "<h1>Jenkins ECR ECS deploy CI/CD</h1>"
 
 
 if __name__ == '__main__':
